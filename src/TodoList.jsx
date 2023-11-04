40d3218b6d5b52ac3641027d15d38282f1119d23
PauloHervatin <120523159+PauloHervatin@users.noreply.github.com>
2023-11-04 10:22:13 -0300

    Enviando projeto para deploy

---
+++ src/TodoList.jsx
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from "react";
+import "./TodoList.css";
+import Icone from './assets/icon.webp';
+
+function TodoList(){
+
+    const listaStorage = localStorage.getItem('Lista');
+
+    const [lista, setLista] = useState(listaStorage ? JSON.parse(listaStorage): []);
+    const [novoItem, setNovoItem] = useState("");
+
+    useEffect(()=>{
+        localStorage.setItem('Lista', JSON.stringify(lista));
+    }, [lista])
+
+    function adicionaItem(form){
+        form.preventDefault();
+        if(!novoItem){
+        return;
+        }
+        setLista([...lista, {text: novoItem, isCompleted: false}]);
+        setNovoItem("");
+        document.getElementById('input-entrada').focus();
+    }
+
+    function clicou(index){
+        const listaAux = [...lista];
+        listaAux[index].isCompleted = ! listaAux[index].isCompleted;
+        setLista(listaAux);
+
+    }
+
+    function deleta(index){
+        const listaAux = [...lista];
+        listaAux.splice(index,1);
+        setLista(listaAux);
+    }
+
+    function deletaTudo(){
+        setLista([]);
+    }
+
+    return (
+        <div>
+            <h1>Lista de Tareas</h1>
+            <form autoComplete="off" onSubmit={adicionaItem}>
+                <input autoComplete="off" id="input-entrada"
+                type="text" 
+                value={novoItem}
+                onChange={(e)=>{setNovoItem(e.target.value)}}
+                placeholder="Adicione uma tarefa"
+                />
+                <button className="add" type="submit">Add</button>
+            </form>
+            <div className="ListaTarefas">
+                <div>{
+                        lista.length <1
+                        ?
+                        <img className="icon" src={Icone}/>
+                        :
+                        lista.map((item, index)=>(
+                            <div 
+                            key={index}
+                            className={item.isCompleted ? "item completo" : "item"}
+                            >
+                                <span onClick={()=> {clicou(index)}}> {item.text} </span>
+                                <button onClick={()=> {deleta(index)}}className="del">Excluir</button>
+                            </div>
+                        ))
+                    }
+                    {
+                        lista.length > 0 && <button onClick={()=> {deletaTudo()}} className="excluirAll">Excluir todos</button>
+                    }
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default TodoList
